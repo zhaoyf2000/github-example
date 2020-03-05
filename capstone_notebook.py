29209ef9fa9fa62d084b43d886740b5e9efaa2ea
zhaoyf2000 <zhaoyf2000@gmail.com>
2020-03-04 21:26:57 -0600

    add a jupyter notebook with simple codes for capstone project

---
+++ capstone_notebook.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# This notebook will be mainly used for the capstone project. The goal of this project is create a jupyter notebook with some simple python codes and push the notebook to github repository.
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+print("Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
+
